3621827863d2a4d2881a4ff33153e21814a3b7e7
hskhan10 <hassan.shahid10@outlook.com>
2023-06-18 14:40:48 -0700

    Replaces 3rd party carousel with native JS image popover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
   const gridContainer = document.querySelector('.grid-container');
   const popoverOverlay = document.querySelector('.popover-overlay');
-  const swiperWrapper = document.querySelector('.swiper-wrapper');
+  const popoverImage = document.querySelector('.popover-image');
+  const popoverCaption = document.querySelector('.popover-caption');
 
   const heartGif = 'https://cdnb.artstation.com/p/assets/images/images/016/235/721/original/christopher-haugen-heart-rotate4.gif';
   // Function to generate a random image URL
@@ -18,7 +19,21 @@ window.addEventListener('DOMContentLoaded', () => {
     return imageUrls[randomIndex];
   }
 
-  function showCustomPopover() {
+  // Function to generate a random English caption
+  function getRandomCaption() {
+    // Replace this array with your own captions
+    const captions = [
+      'The time we went to philly love park and had a random lady take our picture!',
+      'Enjoying birthdays in philly',
+      'Touring the space needle - new and cool!',
+      // Add more captions here
+    ];
+
+    const randomIndex = Math.floor(Math.random() * captions.length);
+    return captions[randomIndex];
+  }
+
+  function showImagePopover(gridItem) {
     // Show popover on grid item click
     gridItem.addEventListener('click', () => {
       const imageUrl = getRandomImageUrl();
@@ -26,15 +41,6 @@ window.addEventListener('DOMContentLoaded', () => {
       popoverOverlay.style.display = 'block';
     });
   }
-
-  // Initialize Swiper carousel
-  const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
 
   // Create grid items and set random images
   for (let i = 0; i < 30; i++) {
@@ -45,26 +51,19 @@ window.addEventListener('DOMContentLoaded', () => {
     if (i === 7 || i == 10 || i == 15) {
       gridItem.style.backgroundImage = `url(${heartGif})`;
       gridItem.style.cursor = 'pointer';
+      
       // Show popover on grid item click
       gridItem.addEventListener('click', () => {
-        swiper.removeAllSlides();
-        for (let j = 0; j < 3; j++) {
-          const imageUrl = getRandomImageUrl();
-          const swiperSlide = document.createElement('div');
-          swiperSlide.classList.add('swiper-slide');
-          swiperSlide.innerHTML = `<img src="${imageUrl}" alt="">`;
-          swiperWrapper.appendChild(swiperSlide);
-        }
-        swiper.update();
-        swiper.slideTo(0, 0);
-        popoverOverlay.style.display = 'block';
+        const caption = getRandomCaption();
+        popoverImage.src = getRandomImageUrl();
+        popoverCaption.textContent = caption;
+        popoverOverlay.style.display = 'flex';
       });
     }
   }
 
   // Hide popover when overlay is clicked
   popoverOverlay.addEventListener('click', () => {
-    swiper.removeAllSlides();
     popoverOverlay.style.display = 'none';
   });
 });
